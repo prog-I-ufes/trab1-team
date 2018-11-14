c9dc8b0a1579139fab3ddb3247841f7a1115f879
VictorDav <44272327+VictorDav@users.noreply.github.com>
2018-11-14 21:38:10 -0200

    Update trabson.c

--- trabson.c
+++ trabson.c
@@ -5,7 +5,52 @@
 #include <stdio.h>
 #include <string.h>
 
+void infoArquivo(FILE *arquivo, int *linhas, int *dados)
+{
+	int iniciaPalavra = 0, linhas = 0, dados = 0;
+	
+	while (!feof(arquivo))
+	{
+		char caracter;
+		
+		fscanf(arquivo, "%c", &caracter);
+		
+		//Conta quantidade de dados por linha
+		if (caracter != ' ' && caracter != '\n') {iniciaPalavra = 1;}
+		
+		if (caracter == ',' && iniciaPalavra == 1) {dados++;}
+		
+		if (caracter == '\n' && iniciaPalavra == 1) {dados++; linhas++;}
+	}
+}
 
-int main(){
-    printf("hello world!\n");
+int ** distEuc(float **vetorEntradas, float **vetorComparar, int *linhasEntrada, int *colunasEntrada, int *linhasComparar, int *colunasComparar)
+{
+	
+}
+
+void alocaMatriz(float **matriz, int *linhas, int *colunas)
+{
+	int i;
+	
+	*matriz = (int **) malloc (*linhas * sizeof(int *));
+	
+	for (i = 0; i < *linhas; i++)
+	{
+		matriz[i] = (int *) malloc (*colunas * sizeof(int));
+	}
+}	
+
+
+int main()
+{
+	FILE *iris, *vowels;
+	
+	iris = fopen("C:\Users\victo\Desktop\Trab. Prog\iris", "r");
+	
+	while(!feof(iris))
+	{
+		
+	}
+	
 }
