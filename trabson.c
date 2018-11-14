7d924c4f7675bfd845f6bb93dc1daa15f5af0b5d
MeninaDoUkulele <44271674+MeninaDoUkulele@users.noreply.github.com>
2018-11-14 21:15:50 -0200

    Add files via upload

---
+++ trabson.c
@@ -0,0 +1,13 @@
+// TRABSON 2018/2 - PROG II; Vidtor Davic && Guana Labriele;
+
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(){
+    printf("hello world!\n");
+
+    return 0;
+}
